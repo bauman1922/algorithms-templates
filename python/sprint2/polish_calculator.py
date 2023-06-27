d2a2038d13ccd9cc3afded11cccbbf39059f2aed
Сергей Саморуков <bauman1922@gmail.com>
2023-06-27 14:49:01 +0300

    После 4 ревью

--- python/sprint2/polish_calculator.py
+++ python/sprint2/polish_calculator.py
@@ -1,5 +1,12 @@
-# ID: 88595712
+# ID: 88601907
 import operator
+
+OPERATIONS = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.floordiv
+}
 
 
 class Stack:
@@ -17,34 +24,23 @@ class Stack:
 
 
 def get_polish_expression(expression, stack=None,
-                          operations=None, converter=None):
-    polish_expression = expression.split()
+                          operations=OPERATIONS, converter=int):
     if stack is None:
         stack = Stack()
-    if operations is None:
-        operations = {
-            '+': operator.add,
-            '-': operator.sub,
-            '*': operator.mul,
-            '/': operator.floordiv
-        }
-    if converter is None:
-        converter = int
-    for value in polish_expression:
+    for value in expression:
         operation = operations.get(value)
         if operation is None:
             try:
                 stack.push(converter(value))
             except ValueError:
                 raise ValueError(f"Некорректное значение: {value}")
-        else:
-            try:
-                operand_2, operand_1 = stack.pop(), stack.pop()
-                stack.push(operation(operand_1, operand_2))
-            except ValueError:
-                raise ValueError("Некорректное выражение")
+            continue
+        operand_2, operand_1 = stack.pop(), stack.pop()
+        stack.push(operation(operand_1, operand_2))
+
     return stack.pop()
 
 
 if __name__ == "__main__":
-    print(get_polish_expression(input()))
+    expression = input().split()
+    print(get_polish_expression(expression))
