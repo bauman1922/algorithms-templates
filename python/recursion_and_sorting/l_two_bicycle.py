cbf68f5d20ad027538e01cf3299b1b60ab31ffdd
Сергей Саморуков <bauman1922@gmail.com>
2023-06-30 19:31:03 +0300

    NEW

--- python/recursion_and_sorting/l_two_bicycle.py
+++ python/recursion_and_sorting/l_two_bicycle.py
@@ -1,9 +1,4 @@
 def binary_search(arr, price, left, right):
-    if right <= left:
-        return -1
-    mid = (left + right) // 2
-    if arr[mid] >= price > arr[mid - 1] < price or mid == 0:
-        return mid + 1
 
 
 
