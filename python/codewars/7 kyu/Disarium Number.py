44af9cad7fb180cdaeff7a1ada6144c64e256555
Сергей Саморуков <bauman1922@gmail.com>
2023-09-28 15:47:33 +0300

    codewars

--- python/codewars/7 kyu/Disarium Number.py
+++ python/codewars/7 kyu/Disarium Number.py
@@ -1,6 +1,7 @@
 """
 Definition
-Disarium number is the number that The sum of its digits powered with their respective positions is equal to the number itself.
+Disarium number is the number that The sum of its digits powered with their 
+respective positions is equal to the number itself.
 
 Task
 Given a number, Find if it is Disarium or not .
